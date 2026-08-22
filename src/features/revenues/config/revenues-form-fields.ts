8f2d99dfdab75c6e7193ba82dfaf39e113adda70
markokosic <marko.kosic.98@gmail.com>
2026-08-22 17:37:05 +0200

    remove revenues module

--- src/features/revenues/config/revenues-form-fields.ts
+++
@@ -1,3 +0,0 @@
-export const REMUNERATION_FORM_FIELDS = {
-  
-}
--- src/features/revenues/hooks/useRevenueFilters.ts
+++
@@ -1,8 +0,0 @@
-import { useUrlFilters } from '@/common/hooks/useUrlFilters';
-import { parseRevenueFilters } from '../utils/revenue-filters.utils';
-
-export const useRevenueFilters = () => {
-  const { getFilter } = useUrlFilters();
-  return parseRevenueFilters(getFilter);
-};
-
